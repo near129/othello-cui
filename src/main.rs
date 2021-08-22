9d1c25d7957bd2cf9f99bdccf9525142a020558c
near129 <>
2021-08-22 21:27:55 +0900

    Update api

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-use std::io::{Write, stdin, stdout};
+use std::io::{stdin, stdout, Write};
 
-use othello::{board::Square, game::Game};
+use othello::{
+    board::Stone,
+    game::Game,
+    players::{Player, RandomPlayer},
+    utils::input_parse,
+};
 
 fn main() {
-    let mut g = Game::new();
-    while !g.finish() {
-        println!("turn: {:?}", g.turn());
-        println!("{}", g.board());
-        let mut input = String::new();
-        print!("Where do you put stone? [a-h][0-7]:  ");
-        stdout().flush().unwrap();
-        stdin().read_line(&mut input).unwrap();
-        println!();
-        let s = Square::parse(input.strip_suffix('\n').unwrap().to_string());
-        if let Ok(s) = s {
-            println!("{:?}", g.put_stone(s.x, s.y));
+    let mut game = Game::default();
+    let mut input = String::new();
+    let mut random_player = RandomPlayer::new(Stone::Whilte);
+    while !game.finished {
+        println!("turn: {:?}", game.turn);
+        println!("{}", game.board);
+        if game.turn == Stone::Whilte {
+            let (x, y) = random_player.find_move(&game.board).unwrap();
+            if let Err(msg) = game.put(x, y) {
+                eprintln!("{}", msg);
+            }
+        } else {
+            print!("Where do you put stone? [a-h][0-7]:  ");
+            stdout().flush().unwrap();
+            stdin().read_line(&mut input).unwrap();
+            println!();
+            input = input.strip_suffix('\n').unwrap().to_string();
+            match input_parse(&input) {
+                Ok((x, y)) => {
+                    if let Err(msg) = game.put(x, y) {
+                        eprintln!("{}", msg);
+                    }
+                }
+                Err(msg) => eprintln!("{}", msg),
+            }
+            input.clear();
         }
     }
     println!("Game finished!!")
